038a161e7271d159912f0a0bd77c676e8792f805
iymarch <iymarch.py@gmail.com>
2021-02-23 21:26:55 +0300

    add models database

--- models/db.py
+++ models/db.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, ForeignKey, Integer, String, Date, DateTime, Enum
 
-from ..database import Base
+from database import Base
+from models.schemas import TypeCall
 
 
 class Contact(Base):
@@ -12,6 +13,20 @@ class Contact(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    phone_number = Column(String)
+    phone_number = Column(String, unique=True)
     email = Column(String, default=None)
-    date_year = Column(Date, default=None)
+    date_birth = Column(Date, default=None)
+    call_log = relationship("CallLog")
+
+
+class CallLog(Base):
+    """ 
+    Database model CallLog
+    """
+    __tablename__ = "call_logs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type_call = Column(Enum(TypeCall)) 
+    time_session = Column(Integer)
+    datetime_call = Column(DateTime)
+    contact = Column(Integer, ForeignKey("contacts.id"))
