eabc2ad59714fd4df28911b51668fdf4b611cafa
iymarch <iymarch.py@gmail.com>
2021-02-23 21:27:31 +0300

    changed initialization application

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI, Path, Cookie
+from fastapi import FastAPI
 from starlette.config import Config
-from enum import Enum
-from typing import Optional
-from pydantic import BaseModel, EmailStr
+
+
+from routers import contact, call_log
+from database import SessionLocal, engine
+from models import db
+
+
+db.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
+app.include_router(contact.router)
+app.include_router(call_log.router)
